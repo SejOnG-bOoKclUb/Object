6d852150a4121b62124517dbf049d1beae6bd318
sgo722 <sgo722@naver.com>
2024-05-01 15:12:12 +0900

    OOP : chapter01_step01

---
+++ Jun/chapter01/src/theater/step01/Audience.java
@@ -0,0 +1,18 @@
+package theater.step01;
+
+/**
+ * Audience : 관람객
+ * 관람객은 소지품을 보관하기 위해 가방을 소지할 수 있다.
+ */
+public class Audience {
+
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
