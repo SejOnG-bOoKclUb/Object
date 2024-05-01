627ba368e659d7c3da8b5936eba91aac31c742a0
jihun <pjh2996@naver.com>
2024-05-01 22:53:05 +0900

    jihun: 1단원 정리, 코드 미완성

---
+++ jihun/reservation/src/domain/Audience.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Audience {
+    private final Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public int buy(int tickets) {
+      return 0;
+    }
+}
